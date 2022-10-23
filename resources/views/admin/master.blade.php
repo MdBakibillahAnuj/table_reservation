2327822966535b14ba40371f7404fea7f7260fe9
MdBakibbillahAnuj <mdbakibillah789@gmail.com>
2022-10-23 23:23:26 +0600

    first commit

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/style.css">
+</head>
+<body>
+
+@include('admin.navbar.top-nav')
+<script src="{{ asset('/') }}assets/js/jquery.min.js"></script>
+<script src="{{ asset('/') }}assets/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('/') }}assets/js/app.js"></script>
+</body>
+</html>
---
+++ resources/views/admin/navbar/top-nav.blade.php
@@ -0,0 +1,31 @@
+
+<nav class="navbar navbar-expand-lg navbar-white ftco_navbar ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+        <a class="navbar-brand" href="/">
+            <img src="{{asset('/')}}assets/img/logo.png" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="oi oi-menu"></span> Menu
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Category</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                <li class="nav-item cta"><a href="#" class="nav-link">Book a table</a></li>
+                @if(!Auth::check())
+                    <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
+                    <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Register</a></li>
+                @else
+                    <li class="nav-item"><a onclick="event.preventDefault(); document.getElementById('logoutForm').submit();" href="#" class="nav-link">[-LogOut-]</a></li>
+                    <form action="{{ route('logout') }}" method="post" id="logoutForm">
+                        @csrf
+                    </form>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- END nav -->
